e3888922c5faf8d1382c4d8276e4d3aadaae054f
enamoria <phanhuykinh1@gmail.com>
2019-04-12 10:53:46 +0700

    evaluation scripts

---
+++ eval.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+
+""" Created on 9:27 AM, 4/10/19
+    @author: ngunhuconchocon
+    @brief: Пролетарии всех стран, соединяйтесь!
+"""
+
+import os
+import sys
+import time
+import logging
+import scipy
+import pysptk
+import subprocess
+import pickle
+import parmap
+
+import pyworld as pw
+import librosa as lbr
+import matplotlib.pyplot as plt
+import numpy as np
+
+from tqdm import tqdm
+from utils import read_dictionary
+from multiprocessing import Pool, cpu_count
+from scipy.stats import describe
+from itertools import repeat
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+cpu_rate = 0.8
+# frame_length = 25  # ms
+# hop_length = 10  # ms
+
+is_toy = False
+
+speaker = "doanngocle_1"
+
+
+def get_data(path):
+    if is_toy:
+        textpath = os.path.join(path, 'txt/text_toy')
+    else:
+        textpath = os.path.join(path, 'txt/text')
+
+    logging.info("Reading data from {}".format(textpath))
+    wavs = []
+    sr = None
+
+    with open(textpath, 'r') as f:
+        for line in tqdm(f.readlines()):
+            filename, text = line.strip().split("|")
+            y_temp, sr = lbr.load(os.path.join(os.path.join(path, 'wav'), filename + ".wav"), sr=None, dtype='double')
+
+            wavs.append([filename, text, y_temp])
+
+    return wavs, sr
+
+
+def _calculate_mean_sd_energy(raw_wav, n_fft, n_hop):
+    """
+    Calculate 1
+    :param _:
+    :return:
+    """
+    energy = []
+
+    for count, i in enumerate(range(0, len(raw_wav) - n_fft, n_hop)):
+        # temp = np.multiply(raw_wav_windowed, raw_wav[i: i + n_fft])
+        energy.append(np.sqrt(np.sum(np.square(raw_wav[i: i + n_fft]))))
+
+    _energy = np.sum(energy) / len(energy)
+    temp = scipy.stats.describe(energy)
+
+    return temp.mean, np.sqrt(temp.variance)
+
+
+def calculate_mean_sd_energy(raw_wavs, sr, frame_length, hop_length):
+    """
+    Calculate mean & sd for whole dataset
+    :param _:
+    :return:
+    """
+    logging.info("Calculating energy data ...")
+
+    n_fft = int(frame_length * sr / 1000)
+    n_hop = int(hop_length * sr / 1000)
+
+    energies = []
+    for wav in tqdm(raw_wavs):
+        energies.append(_calculate_mean_sd_energy(wav, n_fft, n_hop))
+
+    return energies
+
+
+def _calculate_f0(raw_wav, sr, frame_period):
+    f0, _ = pw.harvest(raw_wav, sr, frame_period)
+    return f0
+
+
+def calculate_f0(raw_wavs, sr, frame_period=5, parallel=True):
+    f0s = []
+    logging.info("Calculating f0 ...")
+
+    if not parallel:
+        for wav in raw_wavs:
+            f0s.append(_calculate_f0(wav, sr, frame_period))
+    else:
+        n_worker = int(cpu_count() * cpu_rate)
+        logging.info("{}% resources ({} cpu core(s)) will be used for f0 calculation".format(cpu_rate * 100, n_worker))
+        pool = Pool(n_worker)
+        # f0s = pool.starmap(_calculate_f0, zip(raw_wavs, repeat(sr), repeat(frame_period)))
+        f0s = parmap.starmap(_calculate_f0, list(zip(raw_wavs, repeat(sr), repeat(frame_period))), pm_processes=n_worker, pm_pbar=True)
+
+    return f0s
+
+
+def calculate_mean_sd_f0(f0s):
+    sum_f0s = []
+
+    for i in range(len(f0s)):
+        not_silence_f0 = [value for value in f0s[i] if value != 0]
+        sum_f0 = sum(not_silence_f0) / len(not_silence_f0)
+
+        sum_f0s.append(sum_f0)
+
+    return sum_f0s
+
+
+def _calculate_speaking_rate(raw_wav, text, sr, dictionary):
+    """
+        Speaking rate equals to number of `phonemes / length (sec) in a sentence`
+        `dictionary` will reused pyGenLab resources :D
+
+        Text should be normalized at first
+    :param text:
+    :param length_wav:
+    :param dictionary:
+    :return:
+    """
+    phoneme_count = 0
+    for word in text.split(" "):
+        try:
+            phoneme_count += len([xxx for xxx in dictionary[word] if not xxx.isdigit()])
+        except KeyError as ke:
+            logging.error("{}: {} can't be found in dictionary".format(ke, word))
+
+    length_wav = len(raw_wav) / sr
+    return phoneme_count / length_wav
+
+
+def calculate_speaking_rate(raw_wavs, texts, sr, dictionary):
+    n_worker = int(cpu_count() * cpu_rate)
+    logging.info("{}% resources ({} cpu core(s)) will be used for speaking_rate calculation".format(cpu_rate * 100, n_worker))
+
+    pool = Pool(n_worker)
+    # rates = pool.starmap(_calculate_speaking_rate, zip(raw_wavs, texts, repeat(sr), repeat(dictionary)))
+    rates = parmap.starmap(_calculate_speaking_rate, list(zip(raw_wavs, texts, repeat(sr), repeat(dictionary))), pm_processes=n_worker, pm_pbar=True)
+
+    return rates
+
+
+def _calculate_mean_utt_length(raw_wav, sr):
+    """
+        Calculate utterance length (in second)
+    :param raw_wav:
+    :param sr:
+    :return:
+    """
+    return len(raw_wav) / sr
+
+
+def calculate_mean_utt_length(raw_wavs, sr):
+    n_worker = int(cpu_count() * cpu_rate)
+    logging.info("{}% resources ({} cpu core(s)) will be used for mean utterance length calculation".format(cpu_rate * 100, n_worker))
+    pool = Pool(n_worker)
+
+    # return pool.starmap(_calculate_mean_utt_length, zip(raw_wavs, repeat(sr)))
+    return parmap.starmap(_calculate_mean_utt_length, list(zip(raw_wavs, repeat(sr))), pm_processes=n_worker, pm_pbar=True)
+    # return [_calculate_mean_utt_length(raw_wav, sr) for raw_wav in raw_wavs]
+
+
+def draw_plot(energy_data=None, f0_data=None, speaking_rate_data=None, utt_length=None):
+    logger_eval = logging.getLogger("eval")
+    # energy
+
+    if not energy_data and not f0_data and not speaking_rate_data and not utt_length:
+        logger_eval.critical("None of kwargs is specified. Nothing to plot")
+        return -1
+    else:
+        logger_eval.info("Presenting plot ...")
+
+        if energy_data:
+            xxx = [xx[0] for xx in energy_data]
+            yyy = [xx[1] for xx in energy_data]
+
+            fig, axes = plt.subplots(1, 3, sharey=True)
+            fig.suptitle("Energy data")
+
+            axes[0].scatter(xxx, yyy)
+            axes[1].boxplot(xxx)
+            axes[2].boxplot(yyy)
+
+        if f0_data:
+            fig, axes = plt.subplots(1, 2)
+            fig.suptitle("f0 data")
+
+            axes[0].hist(f0_data, 10)
+            axes[1].boxplot(f0_data)
+
+        if speaking_rate_data:
+            fig, axes = plt.subplots(1, 2)
+            fig.suptitle("speaking_rate: how fast she/he is doing")
+
+            axes[0].hist(speaking_rate_data, 50)
+            axes[1].boxplot(speaking_rate_data)
+
+        if utt_length:
+            fig, axes = plt.subplots(1, 2)
+            fig.suptitle("length of utterance (sec)")
+
+            axes[0].hist(utt_length, 50)
+            axes[1].boxplot(utt_length)
+            pass
+
+    plt.show()
+
+
+def calculate(data, sr, speaker, dictionary_path="vn.dict", output_path="features", frame_length=25, hop_length=10):
+    """
+    Calculate some measurements for speaker evaluation (and his/her synthesized voice)
+    These below attributes will be taken into account:
+        - Mean & SD of `energy` for the whole dataset
+        - Mean & SD of `f0` for `voiced frames` for the whole dataset
+        - Mean Speaking rate ( each = number of phonemes / length (sec) in a sentence) for the whole dataset
+        - Mean utterance length (sec) for the whole dataset
+    Each attribute will be implemented separately. This function packages all of them into a single call
+
+    :param data: a list, which is read from `get_data`: a list of [filename, text, wav]
+    :param sr: sampling rate
+    :param speaker: speaker name
+    :param dictionary_path: path of the phonetic dictionary, used for speaking_rate calculation
+    :param output_path: where to save the `features` object
+    :param frame_length: length of a frame (sec)
+    :param hop_length: stride (sec)
+    :return:
+    """
+    dictionary = read_dictionary(dictionary_path)
+
+    filenames = []
+    texts = []
+    wavs = []
+
+    for i in range(len(data)):
+        filenames.append(data[i][0])
+        texts.append(data[i][1])
+        wavs.append(data[i][2])
+
+    # Energy
+    energies = calculate_mean_sd_energy(wavs, sr, frame_length, hop_length)
+
+    # f0
+    f0s = calculate_f0(wavs, sr, frame_period=10)
+    sum_f0s = calculate_mean_sd_f0(f0s)
+
+    # Speaking rate
+    speaking_rates = calculate_speaking_rate(wavs, texts, sr, dictionary)
+
+    # Utterance length
+    utt_length = calculate_mean_utt_length(wavs, sr)
+
+    # Feature dictionary
+    features = {
+        'energy': energies,
+        'f0': sum_f0s,
+        'speaking_rates': speaking_rates,
+        'utt_length': utt_length
+    }
+
+    feature_output = os.path.join(output_path, speaker)
+    subprocess.call(['mkdir', '-p', output_path])
+
+    with open(feature_output + ".pkl", "wb") as f:
+        pickle.dump(features, f)
+
+    return features
+
+
+if __name__ == "__main__":
+    data, sr = get_data(os.path.join("/data/data/tts/", speaker))
+
+    features = calculate(data, sr, speaker)
+
+    energies, sum_f0s, speaking_rates, utt_length = features['energy'], features['f0'], features['speaking_rates'], features['utt_length']
+
+    # Plot
+    draw_plot(energy_data=energies, f0_data=sum_f0s, speaking_rate_data=speaking_rates, utt_length=utt_length)
+
+    print("Done")
---
+++ utils.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+""" Created on 5:49 PM, 4/10/19
+    @author: ngunhuconchocon
+    @brief: Пролетарии всех стран, соединяйтесь!
+"""
+
+import matplotlib.pyplot as plt
+import logging
+import numpy as np
+
+
+def read_dictionary(dict_path):
+    temp = {}
+    with open(dict_path, "r") as f:
+        for line in f.readlines():
+            line_split = line.strip().split(" ")
+            temp[line_split[0]] = line_split[2:]
+
+    return temp
+
+
+def plot(type_of_plot, data, figure, xlabel=None, ylabel=None, **kwargs):
+    """
+        FORGET IT
+        Drawing plot, support multiple types
+    :param type_of_plot:
+    :param data:
+    :param kwargs:
+    :return:
+    """
+    logger_eval = logging.getLogger("eval")
+
+    if type_of_plot == 'boxplot':
+        axes = figure.gca()
+        axes.boxplot(data)
+
+        if xlabel:
+            figure.xlabel(xlabel)
+        if ylabel:
+            axes.ylabel(ylabel)
+
+        return axes
+    else:
+        logger_eval.critical("The {} plot is not supported yet".format(type_of_plot))
+        return None
+
+
+def label_subplot_with_a_big_subplot(_fig, xlabel=None, ylabel=None):
+    """
+        To name a big figure with a lot of subplot, by adding a big subplot over it and name its x/y axis
+    :param ylabel:
+    :param xlabel:
+    :param _fig:
+    :return:
+    """
+    ax_big = _fig.add_subplot(111, frameon=False)
+    ax_big.spines['top'].set_color('none')
+    ax_big.spines['bottom'].set_color('none')
+    ax_big.spines['left'].set_color('none')
+    ax_big.spines['right'].set_color('none')
+    ax_big.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+
+    if xlabel:
+        ax_big.set_xlabel(xlabel)
+    if ylabel:
+        ax_big.set_ylabel(ylabel)
+
+    return ax_big
+if __name__ == "__main__":
+    #First create some toy data:
+    x = np.linspace(0, 2*np.pi, 400)
+    y = np.sin(x**2)
+
+    #Creates just a figure and only one subplot
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_title('Simple plot')
+
+    #Creates two subplots and unpacks the output array immediately
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    ax1.plot(x, y)
+    ax1.set_title('Sharing Y axis')
+    ax2.scatter(x, y)
+    #
+    #Creates four polar axes, and accesses them through the returned array
+    fig, axes = plt.subplots(2, 2, subplot_kw=dict(polar=True))
+    axes[0, 0].plot(x, y)
+    axes[1, 1].scatter(x, y)
+    #
+    # #Share a X axis with each column of subplots
+    # plt.subplots(2, 2, sharex='col')
+    #
+    # #Share a Y axis with each row of subplots
+    # plt.subplots(2, 2, sharey='row')
+    #
+    # #Share both X and Y axes with all subplots
+    # plt.subplots(2, 2, sharex='all', sharey='all')
+    #
+    # #Note that this is the same as
+    # plt.subplots(2, 2, sharex=True, sharey=True)
+    #
+    # #Creates figure number 10 with a single subplot
+    # #and clears it if it already exists.
+    # fig, ax=plt.subplots(num=10, clear=True)
+
+    plt.show()
+
